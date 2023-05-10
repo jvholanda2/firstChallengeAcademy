dc452f9dbb917af9b59916caf75b8a2c693496b5
João Vitor <jvholanda18@gmail.com>
2023-05-10 19:38:47 -0300

    adciona a funcao de atualizar a task, e a funcao de mudanca de status

--- taskManeger/taskManeger/main.swift
+++ taskManeger/taskManeger/main.swift
@@ -10,16 +10,30 @@ import Foundation
 
 class Task {
     var title: String
-    var descrition: String
+    var description: String
     var status: Bool = false
     var isDeleted: Bool = false
     
     
     init(title: String, description: String){
         self.title = title
-        self.descrition = description
+        self.description = description
+    }
+    
+    func updateTask(task: Task){
+        self.title = task.title
+        self.description = task.description
+        
+        //printTask()
+    }
+    
+    func changeStatus(){
+        self.status = !self.status
     }
 }
+
+
+
 
 class TaskList {
     var taskList:[Task] = []
@@ -30,16 +44,18 @@ class TaskList {
     
     func printTask(){
         taskList.forEach { Task in
-            print("titulo: \(Task.title) | description: \(Task.descrition) | status : \(Task.status)")
+            print("titulo: \(Task.title) | description: \(Task.description) | status : \(Task.status)")
         }
     }
     
+
 }
+
 
 var listTask = TaskList(taskList: [
     Task(title: "Task1", description: "Desc1"),
     Task(title: "Task2", description: "Desc2"),
     Task(title: "Task3", description: "Desc3"),
-                                  ])
+])
 
 
