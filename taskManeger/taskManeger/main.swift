7f8b0a84e7d5d484b64d9cc47b7c0bd338e83e20
João Guilherme <joaogacanuto.ada@gmail.com>
2023-05-10 19:18:35 -0300

    adiciona funciao de print das tasks

--- taskManeger/taskManeger/main.swift
+++ taskManeger/taskManeger/main.swift
@@ -10,17 +10,36 @@ import Foundation
 
 class Task {
     var title: String
-    var descritionTask: String
+    var descrition: String
     var status: Bool = false
-    var isDeleted: Bool
+    var isDeleted: Bool = false
     
     
-    init(title: String, descriptionTaks: String, status: Bool, isDeleted: Bool){
+    init(title: String, description: String){
         self.title = title
-        self.descritionTask = descriptionTaks
-        self.status = status
-        self.isDeleted = isDeleted
+        self.descrition = description
     }
 }
 
-var taskList: [Task] = [Task()]
+class TaskList {
+    var taskList:[Task] = []
+    
+    init(taskList:[Task]){
+        self.taskList = taskList
+    }
+    
+    func printTask(){
+        taskList.forEach { Task in
+            print("titulo: \(Task.title) | description: \(Task.descrition) | status : \(Task.status)")
+        }
+    }
+    
+}
+
+var listTask = TaskList(taskList: [
+    Task(title: "Task1", description: "Desc1"),
+    Task(title: "Task2", description: "Desc2"),
+    Task(title: "Task3", description: "Desc3"),
+                                  ])
+
+
